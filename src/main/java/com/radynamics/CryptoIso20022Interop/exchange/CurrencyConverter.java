930a97907b5563e2ecccf3de75165c620dc0a6bf
radyn_rs <rsteimen@radynamics.com>
2021-12-28 12:49:26 +0100

    add: CurrencyConverterTest

--- src/main/java/com/radynamics/CryptoIso20022Interop/exchange/CurrencyConverter.java
+++ src/main/java/com/radynamics/CryptoIso20022Interop/exchange/CurrencyConverter.java
@@ -5,21 +5,27 @@ import java.math.BigDecimal;
 public class CurrencyConverter {
     private final ExchangeRate[] rates;
 
+    public static final double PRECISION = 100000d;
+
     public CurrencyConverter(ExchangeRate[] rates) {
         this.rates = rates;
     }
 
     public double convert(BigDecimal amount, String sourceCcy, String targetCcy) {
+        if (sourceCcy == null) throw new IllegalArgumentException("Parameter 'sourceCcy' cannot be null");
+        if (sourceCcy.length() == 0) throw new IllegalArgumentException("Parameter 'sourceCcy' cannot be empty");
+        if (targetCcy == null) throw new IllegalArgumentException("Parameter 'targetCcy' cannot be null");
+        if (targetCcy.length() == 0) throw new IllegalArgumentException("Parameter 'targetCcy' cannot be empty");
         if (sourceCcy.equalsIgnoreCase(targetCcy)) {
             return amount.doubleValue();
         }
 
         for (var r : rates) {
             if (r.getCcyFrom().equalsIgnoreCase(sourceCcy) && r.getCcyTo().equalsIgnoreCase(targetCcy)) {
-                return amount.doubleValue() * r.getRate();
+                return Math.round(amount.doubleValue() * r.getRate() * PRECISION) / PRECISION;
             }
             if (r.getCcyFrom().equalsIgnoreCase(targetCcy) && r.getCcyTo().equalsIgnoreCase(sourceCcy)) {
-                return amount.doubleValue() / r.getRate();
+                return Math.round(amount.doubleValue() / r.getRate() * PRECISION) / PRECISION;
             }
         }
 
---
+++ src/test/java/com/radynamics/CryptoIso20022Interop/exchange/CurrencyConverterTest.java
@@ -0,0 +1,56 @@
+package com.radynamics.CryptoIso20022Interop.exchange;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.math.BigDecimal;
+
+import static org.junit.Assert.assertEquals;
+
+public class CurrencyConverterTest {
+    private final CurrencyConverter ccyConverter;
+
+    public CurrencyConverterTest() {
+        ExchangeRate[] rates = {
+                new ExchangeRate("CHF", "TEST", 0.9),
+                new ExchangeRate("EUR", "TEST", 1.1),
+                new ExchangeRate("JPY", "TEST", 0.0088), // sometimes defined with factor 100
+                new ExchangeRate("XXX", "TEST", 0.888888),
+                new ExchangeRate("XXX1", "TEST", 0.88888888),
+        };
+        ccyConverter = new CurrencyConverter(rates);
+    }
+
+    @ParameterizedTest
+    @CsvSource({",", "'',''"})
+    public void convertCcyNullOrEmpty(String sourceCcy, String targetCcy) {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            ccyConverter.convert(BigDecimal.valueOf(100), sourceCcy, targetCcy);
+        });
+    }
+
+    @ParameterizedTest
+    @CsvSource({"\"ABC\",\"CHF\"", "\"CHF\",\"ABC\"", "\"ABC\",\"DEF\"", "\"CHF\",\"EUR\""})
+    public void convertCcyUnknown(String sourceCcy, String targetCcy) {
+        Assertions.assertThrows(RuntimeException.class, () -> {
+            ccyConverter.convert(BigDecimal.valueOf(100), sourceCcy, targetCcy);
+        });
+    }
+
+    @Test
+    public void convert() {
+        assertEquals(90, ccyConverter.convert(BigDecimal.valueOf(100), "CHF", "TEST"), 0);
+        assertEquals(110, ccyConverter.convert(BigDecimal.valueOf(100), "EUR", "TEST"), 0);
+        assertEquals(0.88, ccyConverter.convert(BigDecimal.valueOf(100), "JPY", "TEST"), 0);
+        assertEquals(88.8888, ccyConverter.convert(BigDecimal.valueOf(100), "XXX", "TEST"), 0);
+        assertEquals(88.88889, ccyConverter.convert(BigDecimal.valueOf(100), "XXX1", "TEST"), 0);
+
+        assertEquals(111.11111, ccyConverter.convert(BigDecimal.valueOf(100), "TEST", "CHF"), 0);
+        assertEquals(90.909090, ccyConverter.convert(BigDecimal.valueOf(100), "TEST", "EUR"), 0);
+        assertEquals(11363.63636, ccyConverter.convert(BigDecimal.valueOf(100), "TEST", "JPY"), 0);
+        assertEquals(112.50011, ccyConverter.convert(BigDecimal.valueOf(100), "TEST", "XXX"), 0);
+        assertEquals(112.50000, ccyConverter.convert(BigDecimal.valueOf(100), "TEST", "XXX1"), 0);
+    }
+}
