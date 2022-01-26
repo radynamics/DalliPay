c5d116013172b85f6fb1fb676fad00a4556986f2
radyn_rs <rsteimen@radynamics.com>
2022-01-26 09:15:28 +0100

    chg: remove final

--- src/main/java/com/radynamics/CryptoIso20022Interop/MoneyFormatter.java
+++ src/main/java/com/radynamics/CryptoIso20022Interop/MoneyFormatter.java
@@ -5,15 +5,15 @@ import com.radynamics.CryptoIso20022Interop.ui.Utils;
 import java.math.BigDecimal;
 
 public class MoneyFormatter {
-    public static final String formatLedger(BigDecimal amount, String ccy) {
+    public static String formatLedger(BigDecimal amount, String ccy) {
         return String.format("%s %s", Utils.createFormatLedger().format(amount), ccy);
     }
 
-    public static final String formatFiat(BigDecimal amount, String ccy) {
+    public static String formatFiat(BigDecimal amount, String ccy) {
         return formatFiat(Utils.createFormatFiat().format(amount), ccy);
     }
 
-    public static final String formatFiat(String amount, String ccy) {
+    public static String formatFiat(String amount, String ccy) {
         return String.format("%s %s", amount, ccy);
     }
 }
