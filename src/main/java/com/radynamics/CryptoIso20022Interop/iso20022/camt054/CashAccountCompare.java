ca92afcee647d6eb3b358b0d29d9ff8f25e98e4d
rsteimen <rsteimen@radynamics.com>
2022-03-28 15:54:04 +0200

    add: Camt05400109WriterTest

--- src/main/java/com/radynamics/CryptoIso20022Interop/iso20022/camt054/CashAccountCompare.java
+++ src/main/java/com/radynamics/CryptoIso20022Interop/iso20022/camt054/CashAccountCompare.java
@@ -3,15 +3,37 @@ package com.radynamics.CryptoIso20022Interop.iso20022.camt054;
 import com.radynamics.CryptoIso20022Interop.iso20022.Utils;
 import com.radynamics.CryptoIso20022Interop.iso20022.camt054.camt05400102.generated.CashAccount20;
 import com.radynamics.CryptoIso20022Interop.iso20022.camt054.camt05400104.generated.CashAccount25;
+import com.radynamics.CryptoIso20022Interop.iso20022.camt054.camt05400109.generated.CashAccount41;
 import org.apache.commons.lang3.StringUtils;
 
 public class CashAccountCompare {
+    public static boolean isSame(CashAccount41 first, CashAccount41 second) {
+        return isSame(toCashAccount(first), toCashAccount(second));
+    }
+
     public static boolean isSame(CashAccount25 first, CashAccount25 second) {
         return isSame(toCashAccount(first), toCashAccount(second));
     }
 
     public static boolean isSame(CashAccount20 first, CashAccount20 second) {
         return isSame(toCashAccount(first), toCashAccount(second));
+    }
+
+    private static CashAccount toCashAccount(CashAccount41 ca) {
+        if (ca == null) throw new IllegalArgumentException("Parameter 'ca' cannot be null");
+        return new CashAccount() {
+            {
+                id = ca.getId();
+                idIban = ca.getId() == null ? null : ca.getId().getIBAN();
+                idOthr = ca.getId() == null ? null : ca.getId().getOthr();
+                idOthrId = ca.getId() == null || ca.getId().getOthr() == null ? null : ca.getId().getOthr().getId();
+                ccy = ca.getCcy();
+                nm = ca.getNm();
+                tp = ca.getTp();
+                tpCd = ca.getTp() == null ? null : ca.getTp().getCd();
+                tpPrtry = ca.getTp() == null ? null : ca.getTp().getPrtry();
+            }
+        };
     }
 
     private static CashAccount toCashAccount(CashAccount25 ca) {
---
+++ src/test/java/com/radynamics/CryptoIso20022Interop/iso20022/camt054/Camt05400109WriterTest.java
@@ -0,0 +1,44 @@
+package com.radynamics.CryptoIso20022Interop.iso20022.camt054;
+
+import com.radynamics.CryptoIso20022Interop.exchange.CurrencyConverter;
+import com.radynamics.CryptoIso20022Interop.iso20022.camt054.camt05400109.Camt05400109Writer;
+import com.radynamics.CryptoIso20022Interop.iso20022.camt054.camt05400109.generated.Document;
+import com.radynamics.CryptoIso20022Interop.transformation.TransactionTranslator;
+import org.junit.jupiter.api.Test;
+import org.xmlunit.builder.Input;
+
+import java.time.LocalDateTime;
+
+import static org.junit.Assert.assertThat;
+import static org.xmlunit.matchers.CompareMatcher.isSimilarTo;
+
+public class Camt05400109WriterTest {
+    private static final CamtConverter camtConverter = new CamtConverter(Document.class);
+    private static final String ProductVersion = "0.1.2-SNAPSHOT";
+
+    @Test
+    public void testCreate2PaymentsLedgerCcy() throws Exception {
+        testCreate2Payments("XRP", "camt054/camt.054.001.09_testCreate2Payments.xml");
+    }
+
+    @Test
+    public void testCreate2PaymentsUsd() throws Exception {
+        testCreate2Payments("USD", "camt054/camt.054.001.09_testCreate2Payments.USD.xml");
+    }
+
+    private void testCreate2Payments(String targetCcy, String expectationResourceName) throws Exception {
+        var cryptoInstruction = TestFactory.createTransformInstruction();
+        cryptoInstruction.setTargetCcy(targetCcy);
+
+        var t = new TransactionTranslator(cryptoInstruction, new CurrencyConverter(cryptoInstruction.getExchangeRateProvider().latestRates()));
+        var payments = t.apply(TestFactory.createTransactions(cryptoInstruction.getLedger(), targetCcy));
+
+        var w = new Camt05400109Writer(cryptoInstruction.getLedger(), cryptoInstruction, ProductVersion);
+        w.setIdGenerator(new FixedValueIdGenerator());
+        w.setCreationDate(LocalDateTime.of(2021, 06, 01, 16, 46, 10));
+        var actual = camtConverter.toXml(w.createDocument(payments));
+        var expected = camtConverter.toXml(camtConverter.toDocument(getClass().getClassLoader().getResourceAsStream(expectationResourceName)));
+
+        assertThat(Input.fromByteArray(actual.toByteArray()), isSimilarTo(Input.fromByteArray(expected.toByteArray())));
+    }
+}
