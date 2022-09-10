8e86269ea4c6b20d8a2e259216ca23a2bf6df38b
rsteimen <rsteimen@radynamics.com>
2022-09-10 09:43:33 +0200

    add: GateHub issuer address

--- src/main/java/com/radynamics/CryptoIso20022Interop/cryptoledger/xrpl/StaticWalletInfoProvider.java
+++ src/main/java/com/radynamics/CryptoIso20022Interop/cryptoledger/xrpl/StaticWalletInfoProvider.java
@@ -16,6 +16,7 @@ public class StaticWalletInfoProvider implements WalletInfoProvider {
         this.ledger = ledger;
 
         known.put("rhub8VRN55s94qWKDv6jmDy1pUykJzF3wq", new WalletInfo[]{new WalletInfo("Name", "GateHub", 100)});
+        known.put("rhotcWYdfn6qxhVMbPKGDF3XCKqwXar5J4", new WalletInfo[]{new WalletInfo("Name", "GateHub", 100)});
         known.put("rctArjqVvTHihekzDeecKo6mkTYTUSBNc", new WalletInfo[]{new WalletInfo("Name", "GateHub Fifth", 100)});
         known.put("rvYAfWj5gh67oV6fW32ZzP3Aw4Eubs59B", new WalletInfo[]{new WalletInfo("Name", "Bitstamp", 100)});
         known.put("rsoLo2S1kiGeCcn6hCUXVrCpGMWLrRrLZz", new WalletInfo[]{new WalletInfo("Name", "Sologenic", 100)});
