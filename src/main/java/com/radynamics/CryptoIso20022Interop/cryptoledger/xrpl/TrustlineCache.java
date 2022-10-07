695093678c59e2c66f920782169f471066c90894
rsteimen <rsteimen@radynamics.com>
2022-10-07 08:06:49 +0200

    chg: include ledger and network into key to ensure separation between test/livenet

--- src/main/java/com/radynamics/CryptoIso20022Interop/cryptoledger/xrpl/TrustlineCache.java
+++ src/main/java/com/radynamics/CryptoIso20022Interop/cryptoledger/xrpl/TrustlineCache.java
@@ -1,6 +1,5 @@
 package com.radynamics.CryptoIso20022Interop.cryptoledger.xrpl;
 
-import com.radynamics.CryptoIso20022Interop.cryptoledger.WalletCompare;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -9,7 +8,7 @@ import java.util.HashMap;
 public class TrustlineCache {
     private final static Logger log = LogManager.getLogger(TrustlineCache.class);
     private final Ledger ledger;
-    private final HashMap<Wallet, Trustline[]> items = new HashMap<>();
+    private final HashMap<String, Trustline[]> items = new HashMap<>();
 
     public TrustlineCache(Ledger ledger) {
         if (ledger == null) throw new IllegalArgumentException("Parameter 'ledger' cannot be null");
@@ -28,17 +27,22 @@ public class TrustlineCache {
     }
 
     private Trustline[] find(Wallet wallet) {
+        var key = createKey(wallet);
         for (var item : items.entrySet()) {
-            if (WalletCompare.isSame(item.getKey(), wallet)) {
-                log.trace(String.format("CACHE hit %s (%s trustlines)", item.getKey().getPublicKey(), item.getValue().length));
+            if (key.equals(item.getKey())) {
+                log.trace(String.format("CACHE hit %s (%s trustlines)", key, item.getValue().length));
                 return item.getValue();
             }
         }
         return null;
     }
 
+    private String createKey(Wallet wallet) {
+        return String.format("%s_%s_%s", ledger.getId().textId(), ledger.getNetwork().getUrl(), wallet.getPublicKey());
+    }
+
     private void load(Wallet wallet) {
-        items.put(wallet, ledger.listTrustlines(wallet));
+        items.put(createKey(wallet), ledger.listTrustlines(wallet));
     }
 
     public void clear() {
