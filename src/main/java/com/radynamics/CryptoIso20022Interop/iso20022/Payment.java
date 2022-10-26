b43f756cf455f969923df0367b789456563fac7c
rsteimen <rsteimen@radynamics.com>
2022-10-26 10:27:23 +0200

    chg: remove always true expression

--- src/main/java/com/radynamics/CryptoIso20022Interop/iso20022/Payment.java
+++ src/main/java/com/radynamics/CryptoIso20022Interop/iso20022/Payment.java
@@ -101,7 +101,7 @@ public class Payment {
         }
 
         if (exchangeRate == null) {
-            if (ccy.getIssuer() == null && !ccy.equals(cryptoTrx.getAmount().getCcy())) {
+            if (!ccy.equals(cryptoTrx.getAmount().getCcy())) {
                 // Use ledger native currency if there is no specific issued currency
                 cryptoTrx.setAmount(Money.of(0, new Currency(cryptoTrx.getLedger().getNativeCcySymbol())));
             } else {
