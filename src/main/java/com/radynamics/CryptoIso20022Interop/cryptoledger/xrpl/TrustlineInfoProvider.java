3edc8578058a1d4996cbecb2bddcee6b03a2bdbc
rsteimen <rsteimen@radynamics.com>
2022-10-31 13:13:26 +0100

    chg: move TrustlineInfoProvider.toPercentageText -> CurrencyFormatter.formatTransferFee

--- src/main/java/com/radynamics/CryptoIso20022Interop/cryptoledger/xrpl/TrustlineInfoProvider.java
+++ src/main/java/com/radynamics/CryptoIso20022Interop/cryptoledger/xrpl/TrustlineInfoProvider.java
@@ -5,8 +5,8 @@ import com.radynamics.CryptoIso20022Interop.cryptoledger.Wallet;
 import com.radynamics.CryptoIso20022Interop.cryptoledger.*;
 import com.radynamics.CryptoIso20022Interop.cryptoledger.xrpl.walletinfo.InfoType;
 import com.radynamics.CryptoIso20022Interop.cryptoledger.xrpl.walletinfo.WalletInfoLookupException;
+import com.radynamics.CryptoIso20022Interop.exchange.CurrencyFormatter;
 
-import java.text.DecimalFormat;
 import java.util.ArrayList;
 
 public class TrustlineInfoProvider implements WalletInfoProvider {
@@ -25,19 +25,12 @@ public class TrustlineInfoProvider implements WalletInfoProvider {
         for (var o : trustlineCache.get(WalletConverter.from(wallet))) {
             var limitText = MoneyFormatter.formatFiat(o.getLimit());
             var ccy = o.getLimit().getCcy();
-            var transferFeeText = toPercentageText(ccy.getTransferFee());
+            var transferFeeText = CurrencyFormatter.formatTransferFee(ccy);
             var value = String.format("%s (%s), limit: %s, transfer fee: %s", ccy.getCode(), toText(ccy.getIssuer()), limitText, transferFeeText);
             list.add(new WalletInfo(this, "", value, 50));
         }
 
         return list.toArray(new WalletInfo[0]);
-    }
-
-    private static String toPercentageText(Double value) {
-        var df = DecimalFormat.getInstance();
-        df.setMinimumFractionDigits(2);
-        df.setMaximumFractionDigits(2);
-        return df.format(value * 100) + "%";
     }
 
     private String toText(Wallet wallet) {
