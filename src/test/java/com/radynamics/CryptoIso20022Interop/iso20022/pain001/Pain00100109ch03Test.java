52fe677a79a3e447b2ff4d65a7d446a053a0113a
rsteimen <rsteimen@radynamics.com>
2022-11-21 16:50:43 +0100

    add: Pain00100109ch03Test.pain001ExamplePTXV2

--- src/test/java/com/radynamics/CryptoIso20022Interop/iso20022/pain001/Pain00100109ch03Test.java
+++ src/test/java/com/radynamics/CryptoIso20022Interop/iso20022/pain001/Pain00100109ch03Test.java
@@ -229,4 +229,43 @@ public class Pain00100109ch03Test {
             }});
         }
     }
+
+    @Test
+    public void pain001ExamplePTXV2() throws Exception {
+        var ledger = new TestLedger();
+        var ti = new TransformInstruction(ledger, Config.fallback(ledger), new MemoryAccountMappingSource(ledger));
+        ti.setTargetCcy(ledger.getNativeCcySymbol());
+
+        ExchangeRate[] rates = {
+                new ExchangeRate("USD", ledger.getNativeCcySymbol(), 1, ZonedDateTime.now()),
+        };
+        var r = new Pain001Reader(ledger);
+        var tt = new TransactionTranslator(ti, new CurrencyConverter(rates));
+        var transactions = tt.apply(r.read(getClass().getClassLoader().getResourceAsStream("pain001/pain00100109/ch03/pain_001_Example_PT_X_V2.xml")));
+
+        Assertions.assertNotNull(transactions);
+        Assertions.assertEquals(1, transactions.length);
+
+        var expectedSenderAddress = new Address("MUSTER AG") {{
+            setCity("SELDWYLA");
+            setCountryShort("CH");
+        }};
+        var t = transactions[0];
+        assertNull(t.getId());
+        assertNull(t.getInvoiceId());
+        assertNotNull(t.getMessages());
+        Assertions.assertEquals(0, t.getMessages().length);
+        Assertions.assertEquals(1, t.getStructuredReferences().length);
+        Assertions.assertEquals(ReferenceType.Scor, t.getStructuredReferences()[0].getType());
+        Assertions.assertEquals("RF712348231", t.getStructuredReferences()[0].getUnformatted());
+        Assertion.assertEqualsAccount(t, "CH5481230000001998736", "474-8512-007");
+        Assertion.assertAmtCcy(t, 3421.0, "USD", 3421.0, "TEST");
+        Assertion.assertEquals(t.getSenderAddress(), expectedSenderAddress);
+        Assertion.assertEquals(t.getReceiverAddress(), new Address("Peter Fonda") {{
+            setStreet("Saville Row 4");
+            setZip("EC2R WYK");
+            setCity("London");
+            setCountryShort("GB");
+        }});
+    }
 }
