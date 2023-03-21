318f4430b2742e53692fe46401a33a786cc5012a
rsteimen <rsteimen@radynamics.com>
2023-03-21 13:22:24 +0100

    chg: update known GateHub wallets (https://gatehub.net/legal/xrpl-addresses)

--- src/main/java/com/radynamics/dallipay/cryptoledger/xrpl/StaticWalletInfoProvider.java
+++ src/main/java/com/radynamics/dallipay/cryptoledger/xrpl/StaticWalletInfoProvider.java
@@ -18,13 +18,18 @@ public class StaticWalletInfoProvider implements WalletInfoProvider {
     public StaticWalletInfoProvider(Ledger ledger) {
         this.ledger = ledger;
 
-        known.put("rhub8VRN55s94qWKDv6jmDy1pUykJzF3wq", createWalletInfos("GateHub"));
-        known.put("rhotcWYdfn6qxhVMbPKGDF3XCKqwXar5J4", createWalletInfos("GateHub"));
-        known.put("rctArjqVvTHihekzDeecKo6mkTYTUSBNc", createWalletInfos("GateHub Fifth"));
-        known.put("rchGBxcD1A1C2tdxF6papQYZ8kjRKMYcL", createWalletInfos("GateHub Fifth"));
-        known.put("rcA8X3TVMST1n3CJeAdGk1RdRCHii7N2h", createWalletInfos("GateHub Fifth"));
-        known.put("rcRzGWq6Ng3jeYhqnmM4zcWcUh69hrQ8V", createWalletInfos("GateHub Fifth"));
-        known.put("rcEGREd8NmkKRE8GE424sksyt1tJVFZwu", createWalletInfos("GateHub USDC"));
+        known.put("rhub8VRN55s94qWKDv6jmDy1pUykJzF3wq", createWalletInfos("GateHub")); // USD, EUR
+        known.put("rhotcWYdfn6qxhVMbPKGDF3XCKqwXar5J4", createWalletInfos("GateHub")); // USD, EUR
+        known.put("r4GN9eEoz9K4BhMQXe4H1eYNtvtkwGdt8g", createWalletInfos("GateHub")); // GBP
+        known.put("rctArjqVvTHihekzDeecKo6mkTYTUSBNc", createWalletInfos("GateHub")); // SGB
+        known.put("rchGBxcD1A1C2tdxF6papQYZ8kjRKMYcL", createWalletInfos("GateHub")); // BTC
+        known.put("rcyS4CeCZVYvTiKcxj6Sx32ibKwcDHLds", createWalletInfos("GateHub")); // BCH
+        known.put("rcA8X3TVMST1n3CJeAdGk1RdRCHii7N2h", createWalletInfos("GateHub")); // ETH
+        known.put("rDAN8tzydyNfnNf2bfUQY6iR96UbpvNsze", createWalletInfos("GateHub")); // ETC
+        known.put("rcXY84C4g14iFp6taFXjjQGVeHqSCh9RX", createWalletInfos("GateHub")); // DASH
+        known.put("rcRzGWq6Ng3jeYhqnmM4zcWcUh69hrQ8V", createWalletInfos("GateHub")); // LTC
+        known.put("rcvxE9PS9YBwxtGg1qNeewV6ZB3wGubZq", createWalletInfos("GateHub")); // USDT
+        known.put("rcEGREd8NmkKRE8GE424sksyt1tJVFZwu", createWalletInfos("GateHub")); // USDC
         known.put("rvYAfWj5gh67oV6fW32ZzP3Aw4Eubs59B", createWalletInfos("Bitstamp"));
         known.put("rsoLo2S1kiGeCcn6hCUXVrCpGMWLrRrLZz", createWalletInfos("Sologenic"));
         known.put("rs5hzzF7RzdV2Ub7Fv14Z1ShjuCrnL7FEX", createWalletInfos("STASIS"));
