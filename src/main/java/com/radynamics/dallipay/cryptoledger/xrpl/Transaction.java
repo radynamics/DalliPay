7fd73ce8b9386c869dd30771ea3c7a18a343d6ae
rsteimen <rsteimen@radynamics.com>
2023-04-16 12:29:36 +0200

    chg:  use Wallet interface instead of generic.Wallet in xrpl.Transaction

--- src/main/java/com/radynamics/dallipay/cryptoledger/xrpl/Transaction.java
+++ src/main/java/com/radynamics/dallipay/cryptoledger/xrpl/Transaction.java
@@ -19,8 +19,8 @@ public class Transaction implements com.radynamics.dallipay.cryptoledger.Transac
     private Ledger ledger;
     private Money amt;
     private ZonedDateTime booked;
-    private com.radynamics.dallipay.cryptoledger.generic.Wallet senderWallet;
-    private com.radynamics.dallipay.cryptoledger.generic.Wallet receiverWallet;
+    private com.radynamics.dallipay.cryptoledger.Wallet senderWallet;
+    private com.radynamics.dallipay.cryptoledger.Wallet receiverWallet;
     private String destinationTag;
     private ArrayList<String> messages = new ArrayList<>();
     private ArrayList<StructuredReference> references = new ArrayList<>();
@@ -175,11 +175,11 @@ public class Transaction implements com.radynamics.dallipay.cryptoledger.Transac
         fee = value;
     }
 
-    public void setSender(com.radynamics.dallipay.cryptoledger.generic.Wallet sender) {
+    public void setSender(com.radynamics.dallipay.cryptoledger.Wallet sender) {
         this.senderWallet = sender;
     }
 
-    public void setReceiver(com.radynamics.dallipay.cryptoledger.generic.Wallet receiver) {
+    public void setReceiver(com.radynamics.dallipay.cryptoledger.Wallet receiver) {
         this.receiverWallet = receiver;
     }
 
