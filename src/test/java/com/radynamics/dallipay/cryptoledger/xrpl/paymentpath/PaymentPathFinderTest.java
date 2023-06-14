2771ceae69bedd71ab0883dbdb92a70510dd7896
rsteimen <rsteimen@radynamics.com>
2023-06-14 12:12:11 +0200

    fix: unit tests failed

--- src/test/java/com/radynamics/dallipay/cryptoledger/xrpl/paymentpath/PaymentPathFinderTest.java
+++ src/test/java/com/radynamics/dallipay/cryptoledger/xrpl/paymentpath/PaymentPathFinderTest.java
@@ -109,7 +109,7 @@ public class PaymentPathFinderTest {
         p.setReceiverWallet(receiverWallet);
         p.setAmount(Money.of(20.0, new Currency("CCC")));
         p.setSubmitter(TestFactory.createSubmitter());
-        var actual = new PaymentPathFinder().find(new CurrencyConverter(), p);
+        var actual = createPaymentPathFinderInstance().find(new CurrencyConverter(), p);
 
         Assertions.assertEquals(2, actual.length);
         assertLedgerNativeCcyPath(actual[0]);
@@ -142,7 +142,8 @@ public class PaymentPathFinderTest {
         p.setSenderWallet(senderWallet);
         p.setReceiverWallet(receiverWallet);
         p.setAmount(Money.of(20.0, new Currency("CCC")));
-        var actual = new PaymentPathFinder().find(new CurrencyConverter(), p);
+        p.setSubmitter(TestFactory.createSubmitter());
+        var actual = createPaymentPathFinderInstance().find(new CurrencyConverter(), p);
 
         Assertions.assertEquals(3, actual.length);
         assertLedgerNativeCcyPath(actual[0]);
