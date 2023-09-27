6cc5c88b79628359788d93b2041a5e57cec395d1
rsteimen <rsteimen@radynamics.com>
2023-09-27 10:58:07 +0200

    chg: xahau.Ledger currently only supports ManualRateProvider for exchange rates

--- src/main/java/com/radynamics/dallipay/cryptoledger/xrpl/xahau/Ledger.java
+++ src/main/java/com/radynamics/dallipay/cryptoledger/xrpl/xahau/Ledger.java
@@ -4,6 +4,9 @@ import com.formdev.flatlaf.extras.FlatSVGIcon;
 import com.radynamics.dallipay.cryptoledger.FeeSuggestion;
 import com.radynamics.dallipay.cryptoledger.LedgerId;
 import com.radynamics.dallipay.cryptoledger.NetworkInfo;
+import com.radynamics.dallipay.exchange.ExchangeRateProvider;
+import com.radynamics.dallipay.exchange.ExchangeRateProviderFactory;
+import com.radynamics.dallipay.exchange.ManualRateProvider;
 import okhttp3.HttpUrl;
 
 import javax.swing.*;
@@ -43,6 +46,16 @@ public class Ledger extends com.radynamics.dallipay.cryptoledger.xrpl.Ledger {
         networks[0] = NetworkInfo.createLivenet(HttpUrl.get("https://xahau.network/"), "Mainnet");
         networks[1] = NetworkInfo.createTestnet(HttpUrl.get("https://xahau-test.net/"), "Testnet");
         return networks;
+    }
+
+    @Override
+    public String[] getExchangeRateProviders() {
+        return new String[]{ManualRateProvider.ID};
+    }
+
+    @Override
+    public ExchangeRateProvider getDefaultExchangeRateProvider() {
+        return ExchangeRateProviderFactory.create(ManualRateProvider.ID, this);
     }
 
     @Override
