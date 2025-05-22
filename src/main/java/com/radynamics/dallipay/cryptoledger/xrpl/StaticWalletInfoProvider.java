458db2d8fcfc2f9a66fb5a41df8a28e6294dec11
rsteimen <rsteimen@radynamics.com>
2025-05-22 13:12:14 +0200

    add EUROP ("Schuman Financial")

--- src/main/java/com/radynamics/dallipay/cryptoledger/xrpl/StaticWalletInfoProvider.java
+++ src/main/java/com/radynamics/dallipay/cryptoledger/xrpl/StaticWalletInfoProvider.java
@@ -24,6 +24,7 @@ public class StaticWalletInfoProvider extends com.radynamics.dallipay.cryptoledg
         add("rEn9eRkX25wfGPLysUMAvZ84jAzFNpT5fL", "Stably");
         add("rUN5Zxt3K1AnMRJgEWywDJT8QDMMeLH5ok", "Novatti"); // AUDD
         add("rfgbriwuUN5CREJcaBjnb1FdAqxDujbD7k", "Schuman Financial"); // EUROP
+        add("rMkEuRii9w9uBMQDnWV5AA43gvYZR9JxVK", "Schuman Financial"); // EUROP
         add("rMxCKbEDwqr76QuheSUMdEGf4B9xJ8m5De", "Ripple"); // RLUSD
     }
 }
